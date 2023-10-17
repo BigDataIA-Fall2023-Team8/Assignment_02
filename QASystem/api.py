4f5f242c72e56d920d4ca3b8d5843e6b4a2db82b
SohamD148 <deshpande.soha@northeastern.edu>
2023-10-17 15:52:37 -0400

    Code update

--- QASystem/api.py
+++ QASystem/api.py
@@ -6,17 +6,18 @@ from PyPDF2 import PdfReader
 import openai
 from fastapi.middleware.cors import CORSMiddleware
 import logging
+import uvicorn
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://127.0.0.1"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# origins = ["http://localhost", "http://127.0.0.1"]
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 openai.api_key = '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'  # Replace with your OpenAI API key.
 
