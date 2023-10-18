3b6bfbbe436ea2865151dd4afca978040fe02636
SohamD148 <deshpande.soha@northeastern.edu>
2023-10-18 18:54:14 -0400

    Testing Context

--- QASystem/StreamlitGUI.py
+++ QASystem/StreamlitGUI.py
@@ -1,3 +1,131 @@
+# import streamlit as st
+# import requests
+# import pandas as pd
+
+# st.title("PDF Analyzer: OCR and Q/A System")
+
+# # Sidebar
+# st.sidebar.title("Navigation")
+# menu = ["Home", "OCR and Q/A System", "Document Summary", "About"]
+# choice = st.sidebar.selectbox("Choose a section", menu)
+
+# if st.sidebar.button("Reset Session"):
+#     for key in list(st.session_state.keys()):
+#         del st.session_state[key]
+#     st.experimental_rerun()
+
+# def home_section():
+#     st.header("Welcome to the PDF Analyzer App")
+#     st.write("Use this tool to extract text from PDFs and get answers from the extracted text. "
+#              "Navigate through the sidebar to access different functionalities.")
+
+# def ocr_qa_section():
+#     st.header("OCR and Question/Answer System")
+
+#     if 'pdf_text' not in st.session_state:
+#         st.session_state.pdf_text = ""
+
+#     input_methods = ["Upload a PDF file", "Provide a PDF URL Link"]
+#     input_option = st.selectbox("Select input method", input_methods, index=int(st.session_state.get('input_option_index', 0)))
+#     st.session_state['input_option_index'] = input_methods.index(input_option)
+
+#     FASTAPI_ENDPOINT = "https://fastapi-assignment2-4fb0a78ad873.herokuapp.com"
+
+#     uploaded_file = None  # Initialize uploaded_file as None
+
+#     if input_option == "Upload a PDF file":
+#         uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+#         if uploaded_file:
+#             st.session_state.uploaded_file_name = uploaded_file.name
+#         else:
+#             st.write(f"Previously uploaded file: {st.session_state.get('uploaded_file_name', 'None')}")
+#     else:
+#         url = st.text_input('Provide the PDF URL Link', value=st.session_state.get('url', ''))
+#         st.session_state.url = url
+
+#     ocr_methods = ["Nougat", "PyPDF"]
+#     if 'option_index' not in st.session_state:
+#         st.session_state.option_index = 0
+#     option = st.selectbox("Select OCR method", ocr_methods, index=st.session_state.option_index)
+#     st.session_state.option_index = ocr_methods.index(option)
+
+#     if st.button("Perform OCR") and (uploaded_file or st.session_state.url):
+#         with st.spinner('Performing OCR...'):
+#             if uploaded_file:
+#                 uploaded_file_value = uploaded_file.getvalue()
+#                 if uploaded_file_value:
+#                     files = {"file": uploaded_file_value}
+#                     response = requests.post(f"{FASTAPI_ENDPOINT}/perform-ocr/", files=files, data={"ocr_method": option})
+#                 else:
+#                     st.warning("Please upload a PDF file.")
+#                     return
+#             else:
+#                 if url:
+#                     response = requests.post(f"{FASTAPI_ENDPOINT}/perform-ocr/", data={"url": url, "ocr_method": option})
+#                 else:
+#                     st.warning("Please provide a PDF URL Link.")
+#                     return
+
+#             result = response.json()
+
+#             if "status" in result and result["status"] == "success":
+#                 st.session_state.pdf_text = result["ocr_output"]
+#                 st.session_state.time_taken = result["summary"].get("time_taken_s", "N/A")
+#                 st.session_state.characters_sent = result["summary"].get("input_length", "N/A")
+#                 st.session_state.characters_received = result["summary"].get("output_length", "N/A")
+#                 # Assuming number_of_pages needs to be added to the backend's response, you might want to adjust this accordingly
+#                 st.session_state.number_of_pages = result["summary"].get("number_of_pages", "N/A")
+#                 st.write("OCR Output:")
+#                 st.write(st.session_state.pdf_text)
+
+#                 # Question/Answer System
+#                 if 'question_prompt' not in st.session_state:
+#                     st.session_state.question_prompt = ""
+
+#                 st.session_state.question_prompt = st.text_input('Question about the extracted text', st.session_state.question_prompt)
+
+#                 if st.button("Get Answer") and st.session_state.pdf_text and st.session_state.question_prompt:
+#                     with st.spinner('Finding answer...'):
+#                         response = requests.post(f"{FASTAPI_ENDPOINT}/get-answer/",
+#                                                  data={"question": st.session_state.question_prompt, "context": st.session_state.pdf_text})
+#                         answer_data = response.json()
+#                         if "answer" in answer_data:
+#                             st.write(f"Answer: {answer_data['answer']}")
+#                         else:
+#                             st.warning("Couldn't get an answer. Please try again.")
+#             else:
+#                 st.error(f"OCR Failed. Response: {result}")
+
+
+# def document_summary_section():
+#     st.header("Document Summary")
+    
+#     if 'time_taken' in st.session_state:
+#         summary_data = {
+#             "Metric": ["Time taken for OCR", "Characters sent for OCR", "Characters received after OCR", "Number of pages obtained"],
+#             "Value": [st.session_state.time_taken, st.session_state.characters_sent, st.session_state.characters_received, st.session_state.number_of_pages]
+#         }
+        
+#         summary_df = pd.DataFrame(summary_data)
+#         st.dataframe(summary_df.style.set_properties(**{'text-align': 'left'}))
+#     else:
+#         st.write("Summary of the last document processed will appear here.")
+
+# def about_section():
+#     st.header("About PDF Analyzer")
+#     st.write("This application allows users to extract text from PDF files and ask questions related to the extracted text.")
+
+# # Mapping sections to functions
+# sections = {
+#     "Home": home_section,
+#     "OCR and Q/A System": ocr_qa_section,
+#     "Document Summary": document_summary_section,
+#     "About": about_section
+# }
+
+# # Running the appropriate section
+# sections[choice]()
+
 import streamlit as st
 import requests
 import pandas as pd
@@ -14,6 +142,9 @@ if st.sidebar.button("Reset Session"):
         del st.session_state[key]
     st.experimental_rerun()
 
+def split_into_chunks(text, chunk_size=500):
+    return [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+
 def home_section():
     st.header("Welcome to the PDF Analyzer App")
     st.write("Use this tool to extract text from PDFs and get answers from the extracted text. "
@@ -24,6 +155,7 @@ def ocr_qa_section():
 
     if 'pdf_text' not in st.session_state:
         st.session_state.pdf_text = ""
+        st.session_state.chunks = []
 
     input_methods = ["Upload a PDF file", "Provide a PDF URL Link"]
     input_option = st.selectbox("Select input method", input_methods, index=int(st.session_state.get('input_option_index', 0)))
@@ -31,7 +163,7 @@ def ocr_qa_section():
 
     FASTAPI_ENDPOINT = "https://fastapi-assignment2-4fb0a78ad873.herokuapp.com"
 
-    uploaded_file = None  # Initialize uploaded_file as None
+    uploaded_file = None
 
     if input_option == "Upload a PDF file":
         uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
@@ -44,8 +176,6 @@ def ocr_qa_section():
         st.session_state.url = url
 
     ocr_methods = ["Nougat", "PyPDF"]
-    if 'option_index' not in st.session_state:
-        st.session_state.option_index = 0
     option = st.selectbox("Select OCR method", ocr_methods, index=st.session_state.option_index)
     st.session_state.option_index = ocr_methods.index(option)
 
@@ -56,38 +186,26 @@ def ocr_qa_section():
                 if uploaded_file_value:
                     files = {"file": uploaded_file_value}
                     response = requests.post(f"{FASTAPI_ENDPOINT}/perform-ocr/", files=files, data={"ocr_method": option})
-                else:
-                    st.warning("Please upload a PDF file.")
-                    return
             else:
-                if url:
-                    response = requests.post(f"{FASTAPI_ENDPOINT}/perform-ocr/", data={"url": url, "ocr_method": option})
-                else:
-                    st.warning("Please provide a PDF URL Link.")
-                    return
+                response = requests.post(f"{FASTAPI_ENDPOINT}/perform-ocr/", data={"url": st.session_state.url, "ocr_method": option})
 
             result = response.json()
 
             if "status" in result and result["status"] == "success":
                 st.session_state.pdf_text = result["ocr_output"]
-                st.session_state.time_taken = result["summary"].get("time_taken_s", "N/A")
-                st.session_state.characters_sent = result["summary"].get("input_length", "N/A")
-                st.session_state.characters_received = result["summary"].get("output_length", "N/A")
-                # Assuming number_of_pages needs to be added to the backend's response, you might want to adjust this accordingly
-                st.session_state.number_of_pages = result["summary"].get("number_of_pages", "N/A")
+                st.session_state.chunks = split_into_chunks(st.session_state.pdf_text)
+
+                # Displaying OCR Output
                 st.write("OCR Output:")
                 st.write(st.session_state.pdf_text)
 
                 # Question/Answer System
-                if 'question_prompt' not in st.session_state:
-                    st.session_state.question_prompt = ""
-
-                st.session_state.question_prompt = st.text_input('Question about the extracted text', st.session_state.question_prompt)
-
-                if st.button("Get Answer") and st.session_state.pdf_text and st.session_state.question_prompt:
+                question_prompt = st.text_input('Question about the extracted text')
+                if st.button("Get Answer") and question_prompt:
                     with st.spinner('Finding answer...'):
+                        context_to_send = "\n".join(st.session_state.chunks[-4:])  # Sending the last 4 chunks for context
                         response = requests.post(f"{FASTAPI_ENDPOINT}/get-answer/",
-                                                 data={"question": st.session_state.question_prompt, "context": st.session_state.pdf_text})
+                                                 data={"question": question_prompt, "context": context_to_send})
                         answer_data = response.json()
                         if "answer" in answer_data:
                             st.write(f"Answer: {answer_data['answer']}")
@@ -95,7 +213,6 @@ def ocr_qa_section():
                             st.warning("Couldn't get an answer. Please try again.")
             else:
                 st.error(f"OCR Failed. Response: {result}")
-
 
 def document_summary_section():
     st.header("Document Summary")
--- QASystem/api.py
+++ QASystem/api.py
@@ -1,4 +1,179 @@
-from fastapi import FastAPI, Form, UploadFile, File
+# from fastapi import FastAPI, Form, UploadFile, File
+# import requests
+# import io
+# import time
+# from PyPDF2 import PdfReader
+# import openai
+# from fastapi.middleware.cors import CORSMiddleware
+# import logging
+# import uvicorn
+
+# app = FastAPI()
+
+# origins = [
+#     "http://localhost:8501",
+#     "http://localhost",
+#     "http://127.0.0.1"
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# openai.api_key = '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'  # Replace with your OpenAI API key.
+
+# def get_answer_from_model(prompt, model_name="text-davinci-002"):
+#     response = openai.Completion.create(
+#         engine=model_name,
+#         prompt=prompt,
+#         max_tokens=150
+#     )
+#     return response.choices[0].text.strip()
+
+# def perform_pypdf_ocr(pdf_file):
+#     start_time = time.time()
+#     pdf_text = []
+#     pdf_reader = PdfReader(io.BytesIO(pdf_file))
+#     initial_page_count = len(pdf_reader.pages)
+
+#     for page_num in range(initial_page_count):
+#         page = pdf_reader.pages[page_num]
+#         page_text = page.extract_text()
+#         pdf_text.append(f"Page {page_num + 1}:\n{page_text}\n")
+
+#     extracted_text = '\n'.join(pdf_text)
+#     end_time = time.time()
+#     elapsed_time = end_time - start_time
+
+#     # Create a summary of the operation
+#     summary = {
+#         'Time_Taken_seconds': elapsed_time,
+#         'characters_sent': len(pdf_file),
+#         'characters_received': len(extracted_text),
+#         'initial_page_count': initial_page_count,
+#         'processed_page_count': initial_page_count  # assuming all pages are processed
+#     }
+
+#     #summary_str = "\n\n" + "\n".join([f"{key}: {value}" for key, value in summary.items()])
+#     #extracted_text += summary_str
+
+#     return extracted_text
+
+# def perform_nougat_ocr(pdf_file):
+#     nougat_url = "https://7ee3-35-204-119-38.ngrok-free.app/predict"  # Update as per your configuration
+
+#     # Ensure the file object is in the correct format for requests (bytes).
+#     if not isinstance(pdf_file, bytes):
+#         pdf_file = pdf_file.read()
+    
+#     files = {'file': ("document.pdf", pdf_file)}  # Named tuple to ensure correct handling by API
+    
+#     start_time = time.time()  # Start timing the OCR process
+    
+#     try:
+#         # Send the request to the Nougat OCR API
+#         response = requests.post(nougat_url, files=files)
+#     except requests.RequestException as e:
+#         print(f"Request failed: {e}")
+#         return None, {"error": str(e), "duration": 0, "num_pages": 0}
+    
+#     end_time = time.time()  # End timing
+#     duration = end_time - start_time  # Calculate duration
+    
+
+#     # Check for successful status code
+#     if response.status_code == 200:
+#         # Logic to extract page number information (adjust based on actual API response structure)
+#         ocr_text =  response.json()
+#         num_pages = ocr_text.count("Page:")  # Example logic, adjust as needed
+        
+#         summary = {
+#             "Duration": duration,
+#             "Num of Pages": num_pages,
+#             "Characters sent": len(pdf_file),
+#             "Chars Received": len(ocr_text),
+#         }
+
+#         #summary_str = "\n\n" + "\n".join([f"{key}: {value}" for key, value in summary.items()])
+#         #ocr_text += summary_str
+
+#         return ocr_text  # Return the OCR text and the summary
+#         #return {"ocr_text": ocr_text, "summary": summary}
+#     else:
+#         print(f"Failed to perform OCR. Status code: {response.status_code}, Response: {response.text}")
+#         return None, {"error": response.text, "duration": duration, "num_pages": 0}
+
+# @app.post("/perform-ocr/")
+# async def handle_ocr_request(url: str = Form(None), ocr_method: str = Form(...), file: UploadFile = File(None)):
+#     try:
+#         logging.info(f"Received OCR request: url={url}, ocr_method={ocr_method}, file={file}")
+
+#         # Initialize time and content placeholders
+#         start_time = time.time()
+#         pdf_content = None
+
+#         # Retrieve PDF content
+#         if file:
+#             pdf_content = await file.read()
+#         elif url:
+#             response = requests.get(url)
+#             pdf_content = response.content
+#         else:
+#             return {"status": "error", "message": "Neither a URL nor a file was provided."}
+
+#         # Perform OCR
+#         if ocr_method == "PyPDF":
+#             pdf_text = perform_pypdf_ocr(pdf_content)
+#         elif ocr_method == "Nougat":
+#             pdf_text = perform_nougat_ocr(io.BytesIO(pdf_content))
+#         else:
+#             return {"status": "error", "message": "Invalid OCR method"}
+
+#         # Compute elapsed time and log
+#         elapsed_time = time.time() - start_time
+#         logging.info(f"OCR successful. Method: {ocr_method}, Time taken: {elapsed_time:.2f} s")
+
+#         return {
+#             "status": "success",
+#             "ocr_output": pdf_text,
+#             "summary": {
+#                 "ocr_method": ocr_method,
+#                 "time_taken_s": elapsed_time,
+#                 "input_length": len(pdf_content),
+#                 "output_length": len(pdf_text)
+#             }
+#         }
+
+#     except Exception as e:
+#         logging.error(f"Exception occurred: {str(e)}", exc_info=True)  # Logging the full exception details
+#         return {"status": "error", "message": str(e)}
+
+# @app.post("/get-answer/")
+# def handle_question(question: str = Form(...), context: str = Form(...)):
+#     prompt = f"Given the following document: {context} {question}"
+#     answer = get_answer_from_model(prompt)
+#     return {"answer": answer}
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "FastAPI"}
+
+
+# # def main():
+# #     import uvicorn
+
+# #     uvicorn.run("api:app", port=8504)
+
+# # if __name__ == "__main__":
+# #     main()
+
+# Backend (FastAPI) - Complete Updated Code
+
+from fastapi import FastAPI, Form, UploadFile, File, BackgroundTasks
 import requests
 import io
 import time
@@ -6,7 +181,6 @@ from PyPDF2 import PdfReader
 import openai
 from fastapi.middleware.cors import CORSMiddleware
 import logging
-import uvicorn
 
 app = FastAPI()
 
@@ -24,7 +198,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-openai.api_key = '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'  # Replace with your OpenAI API key.
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+chunks = []
+recent_conversation = []
+MAX_CHUNKS_FOR_QUERY = 4
 
 def get_answer_from_model(prompt, model_name="text-davinci-002"):
     response = openai.Completion.create(
@@ -34,7 +212,10 @@ def get_answer_from_model(prompt, model_name="text-davinci-002"):
     )
     return response.choices[0].text.strip()
 
-def perform_pypdf_ocr(pdf_file):
+async def read_file_content(file):
+    return await file.read()
+
+async def perform_pypdf_ocr(pdf_file):
     start_time = time.time()
     pdf_text = []
     pdf_reader = PdfReader(io.BytesIO(pdf_file))
@@ -49,124 +230,77 @@ def perform_pypdf_ocr(pdf_file):
     end_time = time.time()
     elapsed_time = end_time - start_time
 
-    # Create a summary of the operation
     summary = {
         'Time_Taken_seconds': elapsed_time,
         'characters_sent': len(pdf_file),
         'characters_received': len(extracted_text),
         'initial_page_count': initial_page_count,
-        'processed_page_count': initial_page_count  # assuming all pages are processed
+        'processed_page_count': initial_page_count
     }
-
-    #summary_str = "\n\n" + "\n".join([f"{key}: {value}" for key, value in summary.items()])
-    #extracted_text += summary_str
 
     return extracted_text
 
-def perform_nougat_ocr(pdf_file):
-    nougat_url = "https://7ee3-35-204-119-38.ngrok-free.app/predict"  # Update as per your configuration
-
-    # Ensure the file object is in the correct format for requests (bytes).
+async def perform_nougat_ocr(pdf_file):
+    nougat_url = "https://YOUR_NOUGAT_URL/predict"
     if not isinstance(pdf_file, bytes):
         pdf_file = pdf_file.read()
-    
-    files = {'file': ("document.pdf", pdf_file)}  # Named tuple to ensure correct handling by API
-    
-    start_time = time.time()  # Start timing the OCR process
-    
+    files = {'file': ("document.pdf", pdf_file)}
+    start_time = time.time()
     try:
-        # Send the request to the Nougat OCR API
         response = requests.post(nougat_url, files=files)
     except requests.RequestException as e:
         print(f"Request failed: {e}")
         return None, {"error": str(e), "duration": 0, "num_pages": 0}
-    
-    end_time = time.time()  # End timing
-    duration = end_time - start_time  # Calculate duration
-    
-
-    # Check for successful status code
+    end_time = time.time()
+    duration = end_time - start_time
     if response.status_code == 200:
-        # Logic to extract page number information (adjust based on actual API response structure)
         ocr_text =  response.json()
-        num_pages = ocr_text.count("Page:")  # Example logic, adjust as needed
-        
+        num_pages = ocr_text.count("Page:")
         summary = {
             "Duration": duration,
             "Num of Pages": num_pages,
             "Characters sent": len(pdf_file),
             "Chars Received": len(ocr_text),
         }
-
-        #summary_str = "\n\n" + "\n".join([f"{key}: {value}" for key, value in summary.items()])
-        #ocr_text += summary_str
-
-        return ocr_text  # Return the OCR text and the summary
-        #return {"ocr_text": ocr_text, "summary": summary}
+        return ocr_text
     else:
         print(f"Failed to perform OCR. Status code: {response.status_code}, Response: {response.text}")
         return None, {"error": response.text, "duration": duration, "num_pages": 0}
 
+def split_into_chunks(text):
+    return [text[i:i+500] for i in range(0, len(text), 500)]
+
 @app.post("/perform-ocr/")
 async def handle_ocr_request(url: str = Form(None), ocr_method: str = Form(...), file: UploadFile = File(None)):
-    try:
-        logging.info(f"Received OCR request: url={url}, ocr_method={ocr_method}, file={file}")
+    global chunks
+    if file:
+        pdf_content = await read_file_content(file)
+    elif url:
+        response = requests.get(url)
+        pdf_content = response.content
+    else:
+        return {"status": "error", "message": "Neither a URL nor a file was provided."}
+    
+    if ocr_method == "PyPDF":
+        pdf_text = await perform_pypdf_ocr(pdf_content)
+    elif ocr_method == "Nougat":
+        pdf_text = await perform_nougat_ocr(io.BytesIO(pdf_content))
+    else:
+        return {"status": "error", "message": "Invalid OCR method"}
 
-        # Initialize time and content placeholders
-        start_time = time.time()
-        pdf_content = None
-
-        # Retrieve PDF content
-        if file:
-            pdf_content = await file.read()
-        elif url:
-            response = requests.get(url)
-            pdf_content = response.content
-        else:
-            return {"status": "error", "message": "Neither a URL nor a file was provided."}
-
-        # Perform OCR
-        if ocr_method == "PyPDF":
-            pdf_text = perform_pypdf_ocr(pdf_content)
-        elif ocr_method == "Nougat":
-            pdf_text = perform_nougat_ocr(io.BytesIO(pdf_content))
-        else:
-            return {"status": "error", "message": "Invalid OCR method"}
-
-        # Compute elapsed time and log
-        elapsed_time = time.time() - start_time
-        logging.info(f"OCR successful. Method: {ocr_method}, Time taken: {elapsed_time:.2f} s")
-
-        return {
-            "status": "success",
-            "ocr_output": pdf_text,
-            "summary": {
-                "ocr_method": ocr_method,
-                "time_taken_s": elapsed_time,
-                "input_length": len(pdf_content),
-                "output_length": len(pdf_text)
-            }
-        }
-
-    except Exception as e:
-        logging.error(f"Exception occurred: {str(e)}", exc_info=True)  # Logging the full exception details
-        return {"status": "error", "message": str(e)}
+    chunks.extend(split_into_chunks(pdf_text))
+    return {
+        "status": "success",
+        "ocr_output": pdf_text,
+    }
 
 @app.post("/get-answer/")
-def handle_question(question: str = Form(...), context: str = Form(...)):
-    prompt = f"Given the following document: {context} {question}"
-    answer = get_answer_from_model(prompt)
+def handle_question(question: str = Form(...)):
+    global chunks, recent_conversation
+    context = "\n".join(chunks[-MAX_CHUNKS_FOR_QUERY:]) 
+    context += "\nUser: " + question
+    answer = get_answer_from_model(context)
+    recent_conversation.append({"question": question, "answer": answer})
+    if len(recent_conversation) > MAX_CHUNKS_FOR_QUERY:
+        recent_conversation.pop(0)
     return {"answer": answer}
-
-@app.get("/")
-def read_root():
-    return {"Hello": "FastAPI"}
-
-
-# def main():
-#     import uvicorn
-
-#     uvicorn.run("api:app", port=8504)
-
-# if __name__ == "__main__":
-#     main()
